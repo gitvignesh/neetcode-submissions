f7c030938d9d55b59fbe26aa2c3ebd13281256e3
Vignesh Shenoy K <32541419+gitvignesh@users.noreply.github.com>
2026-07-18 12:00:28 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        var prefix = strs[0]
+
+        for (i in 1 until strs.size) {
+            var j = 0
+
+            while ( j < minOf(prefix.length, strs[i].length)) {
+                if (prefix[j] != strs[i][j]) {
+                    break
+                }
+                j++
+            }
+
+            prefix = prefix.substring(0,j)
+        }
+
+        return prefix
+    }
+}
